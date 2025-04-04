af7600c9d63e75ddbdd27ae301c7ea688d7c499f
mouli.T <chandra369mouli@gmail.com>
2025-04-04 18:17:27 -0400

    WebAPI

---
+++ Backend/Db/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Rest_API.Model;
+
+namespace Web_Rest_API.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Task_Entity> Tasks { get; set; }
+    }
+}
---
+++ Frontend/dist/task-management-frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-FGDXPDA7.mjs";import"./chunk-WZIKR3D4.mjs";import"./chunk-XKEL6EXO.mjs";import"./chunk-24XWOOB4.mjs";import"./chunk-QFIHYKPR.mjs";import"./chunk-M6CLYFVB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
